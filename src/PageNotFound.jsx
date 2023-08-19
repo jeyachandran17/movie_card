34552ac56b1794da1881342b020f927892e03ac9
jeyachandran17 <jeyachandran1733@gmail.com>
2023-08-20 01:02:31 +0530

    added image folder & link image

--- src/PageNotFound.jsx
+++ src/PageNotFound.jsx
@@ -1,7 +1,9 @@
+import NotFoundPage from './image/NotFoundImage.gif'
+
 export function PageNotFound() {
   return (
     <div className='not-fount'>
-      <img src="https://webartdevelopers.com/blog/wp-content/uploads/2020/02/404-Error-Lost-And-Alone.gif" className="not-found-image" alt="Not Found the Page" />
+      <img src={NotFoundPage} className="not-found-image" alt="Not Found the Page" />
     </div>
   );
 }
